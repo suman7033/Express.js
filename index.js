1e1b014bcac16e07e8f83a5f62bf1c05a1575a0f
suman <sumobarnwal@gmail.com>
2024-02-08 11:00:37 +0530

    chat_app

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require("body-parser")
 
-const express = require('express')
-const path=require('path')
-const app = express()
-const port = 3000
+const app=express();
+const port=3000;
 
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-//app.use()
+const data=[];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!sssssdssssss')
+app.get("/",(req,res)=>{
+     res.send(`<form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="post">
+   Type: <input id="message" name="message" type="text" placeHolder="message">
+   <input type="" name="username" id="username"><br/><br/>
+   <button type="submit">Send</button>
+</form>`)
 })
-app.get('/about',(req, res)=>{
-    res.send("about page");
-})
-app.get('/html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'htmltag.html'))
-})
-app.get('/json',(req, res)=>{
-    res.json({"name": 88})
+app.post("/",(req,res)=>{
+   data.push(`{${req.body.username}:${req.body.message}}`)
+   console.log(data);
+   console.log(`${req.body.username}:${req.body.message}`)
+   res.redirect('/')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get("/login",(req,res)=>{
+   res.sendFile(path.join(__dirname,"/login.html"))
+})
+
+app.listen(port, ()=>{
+  console.log(`server is working on port: ${port}`)
 })
