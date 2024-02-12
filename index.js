aefe7798fee6a06370e10adf2ad984ef15ee9d57
suman <sumobarnwal@gmail.com>
2024-02-12 19:35:57 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const path=require('path');
 const bodyParser=require("body-parser")
+const fs=require('fs');
 
 const app=express();
 const port=3000;
@@ -11,18 +12,28 @@ app.use(bodyParser.urlencoded({extended: false}))
 const data=[];
 
 app.get("/",(req,res)=>{
-     res.send(`<form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="post">
+   fs.readFile('username.txt', (err,data)=>{
+      if(err){
+         console.log(err)
+         data='No chat Exists'
+      }
+    
+   res.send(`${data}<form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="post">
    Type: <input id="message" name="message" type="text" placeHolder="message">
-   <input type="" name="username" id="username"><br/><br/>
+   <input type="hidden" name="username" id="username"><br/><br/>
    <button type="submit">Send</button>
 </form>`)
 })
+});
+
 app.post("/",(req,res)=>{
-   data.push(`{${req.body.username}:${req.body.message}}`)
-   console.log(data);
-   console.log(`${req.body.username}:${req.body.message}`)
-   res.redirect('/')
-})
+      console.log(req.body.username);
+      console.log(req.body.message);
+      fs.writeFile("username.txt", `${req.body.username} : ${req.body.message} &nbsp;`,{flag: 'a'},(err)=>
+       err ? console.log(err) : res.redirect("/")
+      )
+   })
+
 
 app.get("/login",(req,res)=>{
    res.sendFile(path.join(__dirname,"/login.html"))
